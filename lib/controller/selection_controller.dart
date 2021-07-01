ce8b70c1c5188dcf74101950690dfe332d02d94a
polydome <polydome@outlook.com>
2021-07-01 11:30:27 +0200

    Implement selection logic

---
+++ lib/controller/selection_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SelectionController extends ChangeNotifier {
+  final Set<int> _selectedIndices = {};
+
+  void select(int index) {
+    _selectedIndices.add(index);
+    notifyListeners();
+  }
+
+  void unselect(int index) {
+    _selectedIndices.remove(index);
+    notifyListeners();
+  }
+
+  void toggle(int index) {
+    if (_selectedIndices.contains(index))
+      _selectedIndices.remove(index);
+    else
+      _selectedIndices.add(index);
+    notifyListeners();
+  }
+
+  void unselectAll() {
+    _selectedIndices.clear();
+    notifyListeners();
+  }
+
+  bool isSelected(int index) => _selectedIndices.contains(index);
+}
--- lib/widget/note_list.dart
+++ lib/widget/note_list.dart
@@ -1,48 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
+import 'package:notty/controller/selection_controller.dart';
+import 'package:provider/provider.dart';
 
 import '../models/note.dart';
 
 class NoteCard extends StatelessWidget {
   final Note note;
   final bool selected;
+  final void Function()? onTap;
 
   const NoteCard({
     Key? key,
     required this.note,
     this.selected = false,
+    this.onTap,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-            border: Border.all(
-              color: selected ? Colors.red : Colors.transparent,
-              width: 2
-            ),
-            borderRadius: BorderRadius.circular(6)),
-        child: Card(
-          margin: EdgeInsets.zero,
-          child: Padding(
-            padding: const EdgeInsets.all(6),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      note.content,
-                      style: Theme.of(context).textTheme.bodyText1,
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: onTap,
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                  color: selected ? Colors.red : Colors.transparent, width: 2),
+              borderRadius: BorderRadius.circular(6)),
+          child: Card(
+            margin: EdgeInsets.zero,
+            child: Padding(
+              padding: const EdgeInsets.all(6),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        note.content,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
                     ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: DateTimeField(dateTime: note.dateCreated),
-                )
-              ],
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: DateTimeField(dateTime: note.dateCreated),
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -71,14 +76,35 @@ class DateTimeField extends StatelessWidget {
   }
 }
 
-class NotesList extends StatelessWidget {
+class NotesList extends StatefulWidget {
   final Stream<List<Note>> notes;
-  final ScrollController _scrollController = ScrollController();
 
   NotesList(this.notes);
 
+  @override
+  State<NotesList> createState() => _NotesListState();
+}
+
+class _NotesListState extends State<NotesList> {
+  final ScrollController _scrollController = ScrollController();
+  final SelectionController _selectionController = SelectionController();
+  bool _selectionMode = true;
+
   Widget Function(BuildContext ctx, int index) buildRow(List<Note> notes) =>
-      (BuildContext ctx, int index) => NoteCard(note: notes[index]);
+      (BuildContext ctx, int index) =>
+        Consumer<SelectionController>(
+          builder: (context, selection, child) => NoteCard(
+            note: notes[index],
+            selected: selection.isSelected(index),
+            onTap: () {
+              setState(() {
+                if (_selectionMode) {
+                  selection.toggle(index);
+                }
+              });
+            },
+          ),
+        );
 
   void scrollToBottom() {
     _scrollController.animateTo(_scrollController.position.maxScrollExtent,
@@ -99,14 +125,16 @@ class NotesList extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(10),
           child: StreamBuilder<List<Note>>(
-              stream: notes,
-              builder: (context, snapshot) {
-                return ListView.builder(
-                  itemBuilder: buildRow(snapshot.data ?? []),
-                  itemCount: snapshot.hasData ? snapshot.data!.length : 0,
-                  controller: _scrollController,
-                );
-              }),
+              stream: widget.notes,
+              builder: (context, snapshot) =>
+                    ChangeNotifierProvider(
+                      create: (context) => _selectionController,
+                      child: ListView.builder(
+                        itemBuilder: buildRow(snapshot.data ?? []),
+                        itemCount: snapshot.hasData ? snapshot.data!.length : 0,
+                        controller: _scrollController,
+                      ),
+                    )),
         ));
   }
 }
