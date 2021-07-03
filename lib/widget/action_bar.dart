b18f318790731a7223f7d415cf4a19020f2c8512
polydome <polydome@outlook.com>
2021-07-03 16:42:32 +0200

    Implement action bar slide animation

--- lib/widget/action_bar.dart
+++ lib/widget/action_bar.dart
@@ -1,36 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:notty/widget/bottom_bar.dart';
 
-class ActionBar extends StatelessWidget {
+class ActionBar extends StatefulWidget {
   final bool visible;
   final void Function()? onCancel;
   final void Function()? onAction;
 
-  const ActionBar({Key? key, this.visible = true, this.onCancel, this.onAction}) : super(key: key);
+  const ActionBar({Key? key, this.visible = true, this.onCancel, this.onAction})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Visibility(
-    visible: visible,
-    child: Material(
-      color: Theme.of(context).colorScheme.primary,
-      child: Padding(
-        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-        child: Container(
-          height: 50,
-          width: double.infinity,
+  State<ActionBar> createState() => _ActionBarState();
+}
+
+class _ActionBarState extends State<ActionBar>
+    with SingleTickerProviderStateMixin {
+  bool _visible = false;
+
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 100),
+    vsync: this,
+  )..addStatusListener((status) {
+      setState(() {
+        switch (status) {
+          case AnimationStatus.forward:
+            _visible = true;
+            break;
+          case AnimationStatus.dismissed:
+            _visible = false;
+            break;
+          default:
+        }
+      });
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.visible)
+      _controller.forward();
+    else
+      _controller.reverse();
+
+    return Visibility(
+      visible: _visible,
+      child: SlideTransition(
+        position: Tween<Offset>(begin: Offset(0, -1), end: Offset.zero)
+            .animate(_controller),
+        child: Material(
+          color: Theme.of(context).colorScheme.primary,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                BottomBarButton(icon: Icons.close, onPressed: onCancel,),
-                BottomBarButton(icon: Icons.delete, onPressed: onAction,)
-              ],
+            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    BottomBarButton(
+                      icon: Icons.close,
+                      onPressed: widget.onCancel,
+                    ),
+                    BottomBarButton(
+                      icon: Icons.delete,
+                      onPressed: widget.onAction,
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
